fced0580196373376764d3c67ef59c8d063719de
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-07 10:16:08 +0200

    Commit UD05 08

---
+++ C2_UD05_08/src/UD05_08_App.java
@@ -0,0 +1,16 @@
+
+public class UD05_08_App {
+
+	public static void main(String[] args) {
+		
+//		8) Haz el mismo ejercicio anterior con un bucle for.
+
+//		Variables
+		
+//		Programa
+		for(int cont = 1; cont <= 100; cont++) {
+			System.out.println(cont);
+		}
+	}
+
+}
