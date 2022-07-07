f4446ebd86d228cf27a8004be8910108d2dd2fe6
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-07 10:13:23 +0200

    Commit UD05 07

---
+++ C2_UD05_07/src/UD05_07_App.java
@@ -0,0 +1,18 @@
+
+public class UD05_07_App {
+
+	public static void main(String[] args) {
+		
+//		7) Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+
+//		Variables
+		int cont = 1;
+		
+//		Programa
+		while(cont <= 100) {
+			System.out.println(cont);
+			cont++;
+		}
+	}
+
+}
