21dfb8a4fbfd2a7f5e584b58b38776f00e5418bb
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-07 10:20:51 +0200

    Commit UD05 09

---
+++ C2_UD05_09/src/UD05_09_App.java
@@ -0,0 +1,20 @@
+
+public class UD05_09_App {
+
+	public static void main(String[] args) {
+		
+//		9) Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. Utiliza el bucle
+//		que desees.
+		
+//		Variables
+		
+//		Programa
+		for(int cont = 1; cont <= 100; cont++) {
+			if(cont%2 == 0 || cont%3 == 0) {
+				System.out.println(cont);
+			}
+		}
+
+	}
+
+}
