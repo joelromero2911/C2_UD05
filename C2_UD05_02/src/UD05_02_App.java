7b2f12c96d1352bce9ae5b56d065da61d8c451ab
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-07 09:39:51 +0200

    Commit UD05 02

---
+++ C2_UD05_02/src/UD05_02_App.java
@@ -0,0 +1,17 @@
+
+public class UD05_02_App {
+
+	public static void main(String[] args) {
+		
+//		2) Declara un String que contenga tu nombre,después muestra un mensaje de bienvenida
+//		por consola. Por ejemplo: si introduzco "Fernando", me aparezca "Bienvenido Fernando".
+		
+//		Variables
+		String nombre = "Joel";
+		
+//		Programa
+		System.out.println("Bienvenido " + nombre);
+
+	}
+
+}
